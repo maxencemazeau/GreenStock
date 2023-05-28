216c08c4dc5136f1ced80aa2db559d61caf1bc49
unknown <maxencemazeau@outlook.fr>
2023-05-28 16:38:25 -0400

    Header and footer + start landing page

--- src/components/Navabar/Footer.tsx
+++ src/components/Navabar/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
       <Typography color="blue-gray" className="font-normal">
-        &copy; 2023 Material Tailwind
+        &copy; 2023 GreenStock
       </Typography>
       <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
         <li>
