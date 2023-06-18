91076a39b2d43abeab9853911882edf9b2613403
maxencemazeau <89794067+maxencemazeau@users.noreply.github.com>
2023-06-18 19:46:33 -0400

    Section profil fini

--- src/page/Profil/Profil.tsx
+++ src/page/Profil/Profil.tsx
@@ -1,15 +1,16 @@
-import Reqct from "react"
-import Information from "../../components/Profil/Information"
-import Historique from "../../components/Profil/Historique"
+import React from "react";
+import Information from "../../components/Profil/Information";
+import Historique from "../../components/Profil/Historique";
 
-
-export default function Profil(){
-
-
-    return(
-       <>
+export default function Profil() {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-4">
         <Information />
-        <Historique/>
-       </>
-    )
+      </div>
+      <div className="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-4">
+        <Historique />
+      </div>
+    </div>
+  );
 }
